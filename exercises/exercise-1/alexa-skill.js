789b9e38427f6a81de76a68f93a6c7d092f2c21e
clickstreamer <clickstreamer@users.noreply.github.com>
2017-08-02 22:53:32 +0200

    Update alexa-skill.js

--- exercises/exercise-1/alexa-skill.js
+++ exercises/exercise-1/alexa-skill.js
@@ -11,7 +11,7 @@
  * either express or implied.  See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 var Alexa = require('alexa-sdk');   //Alexa SDK
 
 var states = {
@@ -51,6 +51,16 @@ var languageStrings = {
     }
 };
 
+// Create default handlers
+var newSessionHandlers = {
+    'LaunchRequest': function () {
+        //Skill was launched
+
+        //TODO: Say Hello!
+        this.emit(':tell', "Hello NAME");
+    }
+};
+
 var main = function (event) {
     console.log('ALEXA Event', event.request.type + '!');
 
@@ -69,7 +79,7 @@ var main = function (event) {
                     });
                 alexaSDK.APP_ID = APP_ID;
                 alexaSDK.resources = languageStrings;
-
+                alexaSDK.registerHandlers(newSessionHandlers);
                 return alexaSDK.execute();
             } catch (err) {
                 console.log(err);
